42cf228955def15c190cf8d6a06c9b684612d7ee
Candice Weaver <68248078+CandiceWeaver@users.noreply.github.com>
2021-09-06 17:13:13 +0100

    built and complete

---
+++ build/precache-manifest.a35de04672931bd438e91bc2009c55d9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ed853f22c6a401013a76e001f6cda155",
+    "url": "/index.html"
+  },
+  {
+    "revision": "33d1900dfec148bea138",
+    "url": "/static/css/main.f98d69af.chunk.css"
+  },
+  {
+    "revision": "ca77627b6a30990c1a89",
+    "url": "/static/js/2.3c152af1.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.3c152af1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "33d1900dfec148bea138",
+    "url": "/static/js/main.9f2b5692.chunk.js"
+  },
+  {
+    "revision": "f13d5fb20258b992088c",
+    "url": "/static/js/runtime-main.a6b2f021.js"
+  }
+]);
